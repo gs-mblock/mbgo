5d404aaf1ed558a0a40a6ed2b26d90c3929a7919
liamA <37907909@qq.com>
2019-08-07 18:24:58 +0800

    fix: 格式化

--- lib/distribution/dbid/dataid_test.go
+++ lib/distribution/dbid/dataid_test.go
@@ -10,52 +10,59 @@ import (
 )
 
 func Test_DataID(t *testing.T) {
-	//test 
-	mid  := DataID(4,1)
+	//test
+	mid := DataID(4, 1)
 	println("[Test] get value", mid)
-	if mid  > 0 {
+	if mid > 0 {
 		t.Log("Test_DataID 通过")
 	}
 }
 
 func Test_IDtoTime(t *testing.T) {
-	//test 
-	mid  := DataID(4,1)
+	//test
+	mid := DataID(4, 1)
 	println("[Test] get value", mid)
-	if mid  > 0 {
+	if mid > 0 {
 		t.Log("Test_DataID 通过")
 	}
 	timeid := IDtoTime(mid)
-	formatTimeStr1:=time.Unix(timeid,0).Format("2006-01-02 15:04:05")
-	println("time S1:",formatTimeStr1)
+	formatTimeStr1 := time.Unix(timeid, 0).Format("2006-01-02 15:04:05")
+	println("time S1:", formatTimeStr1)
 }
 
 func Test_Time2(t *testing.T) {
 	//从字符串转为时间戳，第一个参数是格式，第二个是要转换的时间字符串
 	tm2, _ := time.Parse("2006-01-02 15:04:05", "2019-01-01 00:00:00") // 0时区
-	t1 := tm2.Unix() // 1546300800
-	t2 := tm2.UnixNano() // 1546300800000000000
-	println("time U1:",t1)
-	println("time U2:",t2)
-	formatTimeStr1:=time.Unix(t1,0).Format("2006-01-02 15:04:05")
-	println("time S1:",formatTimeStr1) // 2019-01-01 08:00:00
+	t1 := tm2.Unix()                                                   // 1546300800
+	t2 := tm2.UnixNano()                                               // 1546300800000000000
+	println("time U1:", t1)
+	println("time U2:", t2)
+	formatTimeStr1 := time.Unix(t1, 0).Format("2006-01-02 15:04:05")
+	println("time S1:", formatTimeStr1) // 2019-01-01 08:00:00
 }
 
 func Test_Time(t *testing.T) {
 	t1 := time.Now().Unix()
 	t2 := time.Now().UnixNano()
 	tu2 := time.Now().UTC().Unix() // 时间戳 不分时区
-	println("time U0:",tu2)
-	println("time U1:",t1)
+	println("time U0:", tu2)
+	println("time U1:", t1)
 
-	println("time N0:",t2)
-	formatTimeStr1:=time.Unix(t1,0).Format("2006-01-02 15:04:05")
-	formatTimeStr2:=time.Unix(t1,0).UTC().Format("2006-01-02 15:04:05") // 0时区
-	println("time S1:",formatTimeStr1)
-	println("time S2:",formatTimeStr2)
+	println("time N0:", t2)
+	formatTimeStr1 := time.Unix(t1, 0).Format("2006-01-02 15:04:05")
+	formatTimeStr2 := time.Unix(t1, 0).UTC().Format("2006-01-02 15:04:05") // 0时区
+	println("time S1:", formatTimeStr1)
+	println("time S2:", formatTimeStr2)
 
-	tz1 :=  time.Now().UTC().Format("2006-01-02 15:04:05") // 0时区
-	tz2 :=  time.Now().Format("2006-01-02 15:04:05") 
-	println("time Z1:",tz1)
-	println("time Z1:",tz2)
+	tz1 := time.Now().UTC().Format("2006-01-02 15:04:05") // 0时区
+	tz2 := time.Now().Format("2006-01-02 15:04:05")
+	println("time Z1:", tz1)
+	println("time Z1:", tz2)
+}
+
+func Test_IntToTime(t *testing.T) {
+	var t1 int64 = 1565009204
+	formatTimeStr1 := time.Unix(t1, 0).Format("2006-01-02 15:04:05")
+
+	println(formatTimeStr1)
 }
